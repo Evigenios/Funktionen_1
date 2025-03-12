7ac9b31372b63fb15b071a62888b5da882636093
Evigenios <e.chontzidis@web.de>
2025-03-12 11:04:34 +0100

    Aufgabe2

--- printNumber.dart
+++ printNumber.dart
@@ -1,9 +1,18 @@
-printNumber(int zahl) {
+void printNumber(int zahl) {
   print(zahl);
 }
 
+void triplePrint(String text) {
+  for (int i = 0; i < 3; i++) {
+    print(text);
+  }
+}
+
 void main() {
-  printNumber(12);
+  printNumber(12); //aufgabe 1
   printNumber(22);
   printNumber(2);
+
+  triplePrint("Hallo");//Aufgabe2
+  triplePrint("Welt");
 }
