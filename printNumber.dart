2929229b535c5c9bb75be41ed1ba90c7e9f1541e
Evigenios <e.chontzidis@web.de>
2025-03-12 11:26:39 +0100

    Aufgabe3

--- printNumber.dart
+++ printNumber.dart
@@ -8,11 +8,19 @@ void triplePrint(String text) {
   }
 }
 
+void reverseInitials(String vorname, String nachname) {
+  String vn = vorname[vorname.length - 1].toUpperCase();
+  String nn = nachname[nachname.length - 1].toUpperCase();
+  print("${vn}.${nn}.");
+}
+
 void main() {
   printNumber(12); //aufgabe 1
   printNumber(22);
   printNumber(2);
 
-  triplePrint("Hallo");//Aufgabe2
+  triplePrint("Hallo"); //Aufgabe2
   triplePrint("Welt");
+
+  reverseInitials("Evi", "Cho");//Aufgabe3
 }
